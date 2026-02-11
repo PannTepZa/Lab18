db5510ce84cd193466a5388a00d9a65689abaec9
PannTepZa <pannkitina@gmail.com>
2026-02-12 01:06:45 +0700

    lab18_2.cpp completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,16 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2){
+    double left   = max(R1->x, R2->x);
+    double right  = min(R1->x + R1->w, R2->x + R2->w);
 
+    double top    = min(R1->y, R2->y);
+    double bottom = max(R1->y - R1->h, R2->y - R2->h);
 
+    double width = right - left;
+    double height = top - bottom;
 
+    if (width <= 0 || height <= 0) return 0;
+    return width * height;
 }
